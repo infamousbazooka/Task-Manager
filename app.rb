647fed4ab462ea6e2af5233d0e64965cd08dcc74
infamousbazooka <magnusfernandes1295@gmail.com>
2017-07-25 23:20:08 +0530

    Adding Tasks

--- app.rb
+++ app.rb
@@ -3,13 +3,18 @@ require 'sinatra/activerecord'
 
 set :database, "sqlite3:blog.db"
 
-class Tasks < ActiveRecord::Base
+class Task < ActiveRecord::Base
 end
 
 get '/' do
+    @tasks = Task.all
     erb :index
 end
 
 post '/' do
-    @task = param[:title]
+    # @task = Task.new(params[:title])
+    # @task.save
+    # Task.create params[:title]
+    Task.create(:title => params[:title])
+    redirect "/"
 end
