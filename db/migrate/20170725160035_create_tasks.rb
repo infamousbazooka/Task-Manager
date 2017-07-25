badedf53d324846b5d4b15f30a34da0afa27f067
infamousbazooka <magnusfernandes1295@gmail.com>
2017-07-25 21:50:12 +0530

    Gemfile Created

--- db/migrate/20170725160035_create_tasks.rb
+++ db/migrate/20170725160035_create_tasks.rb
@@ -1,4 +1,11 @@
 class CreateTasks < ActiveRecord::Migration[5.1]
-  def change
-  end
+    def self.up
+        create_table :tasks do |t|
+            t.string :title
+        end
+    end
+
+    def self.down
+        drop_table :tasks
+    end
 end
