4f876573f280b9b4ba188eb0fd29520e64a24e6a
infamousbazooka <magnusfernandes1295@gmail.com>
2017-07-25 22:05:18 +0530

    Home Template

--- app.rb
+++ app.rb
@@ -2,3 +2,14 @@ require 'sinatra'
 require 'sinatra/activerecord'
 
 set :database, "sqlite3:blog.db"
+
+class Tasks < ActiveRecord::Base
+end
+
+get '/' do
+    erb :index
+end
+
+post '/' do
+    @task = param[:title]
+end
